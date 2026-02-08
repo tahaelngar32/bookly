94f93e5db06233ee48ab1469a2f940f01110a800
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-08 18:35:38 +0200

    sliding_text_add

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,21 +1,48 @@
+import 'package:bookly1/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly1/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  //SingleTickerProviderStateMixin  توقيت او عداد
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    );
+    slidingAnimation = Tween<Offset>(begin: Offset(0, 2), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
+  @override
+  void dispose() {
+    animationController.dispose();  // مهمه لعدم استهلاك تادتا
+    super.dispose();
+  }
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Text(
-          'read free books',
-          textAlign: TextAlign.center,
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
 }
+
+
