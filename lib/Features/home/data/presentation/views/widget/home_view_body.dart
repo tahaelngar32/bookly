8358c84ae3e6dbe424795e9dd92ea10f30571b69
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-10 18:35:01 +0200

    best Seller Bart1

--- lib/Features/home/data/presentation/views/widget/home_view_body.dart
+++ lib/Features/home/data/presentation/views/widget/home_view_body.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:bookly1/core/utils/assets.dart';
 import 'package:bookly1/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
@@ -12,13 +15,42 @@ class HomeViewBody extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [   
+        children: [
           CustomAppbar(),
           FeatureBooksListView(),
-          SizedBox(height: 50,),
+          SizedBox(
+            height: 50,
+          ),
           Text(
             'Best Seller',
             style: Styles.titleMadim,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio:2.5/4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.testImage),
+                    fit: BoxFit.fill,
+                  )),
+            ),
           )
         ],
       ),
