0203e9a36a1244d6587b6ba218fffc61bc1540ae
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-12 22:08:47 +0200

    boook details part one

--- lib/Features/home/data/presentation/views/widget/book_details_view_body.dart
+++ lib/Features/home/data/presentation/views/widget/book_details_view_body.dart
@@ -1,21 +1,46 @@
+import 'package:bookly1/Features/home/data/presentation/views/widget/book_reating.dart';
 import 'package:bookly1/Features/home/data/presentation/views/widget/custom_app_bar_book_details.dart';
 import 'package:bookly1/Features/home/data/presentation/views/widget/custom_book_image.dart';
+import 'package:bookly1/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
-    
+
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
           const CustomAppBarBookDetails(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width*.17),
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
             child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.style30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Styles.style18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+                color: Colors.white.withOpacity(.7)),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookReating(
+            mainAxisAlignment: MainAxisAlignment.center,
           )
         ],
       ),
--- lib/Features/home/data/presentation/views/widget/book_reating.dart
+++ lib/Features/home/data/presentation/views/widget/book_reating.dart
@@ -2,20 +2,31 @@ import 'package:bookly1/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookReating extends StatelessWidget {
-  const BookReating({super.key});
-
+  const BookReating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Icon(Icons.star_sharp, color: Color(0xffFFDD4F)),
-        const Text(
-          '4.8',
-          style: Styles.style14,
+        Icon(Icons.star_sharp, color: Color(0xffFFDD4F)),
+        SizedBox(
+          width: 6,
         ),
         Text(
-          '(2390)',
-          style: Styles.style14.copyWith(color: const Color(0xff707070)),
+          '4.8',
+          style: Styles.style16,
+        ),
+        SizedBox(
+          width: 3,
+        ),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(2390)',
+            style: Styles.style14,
+          ),
         ),
       ],
     );
