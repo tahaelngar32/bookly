099c6711a181611b3030dd7b70990e94b96a412f
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-13 11:13:14 +0200

    add custom scroll view

--- lib/Features/home/data/presentation/views/widget/book_details_view_body.dart
+++ lib/Features/home/data/presentation/views/widget/book_details_view_body.dart
@@ -13,61 +13,71 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const CustomAppBarBookDetails(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          const Text(
-            'The Jungle Book',
-            style: Styles.style30,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Text(
-            'The Jungle Book',
-            style: Styles.style18.copyWith(
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w500,
-                color: Colors.white.withOpacity(.7)),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookReating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const BooksAction(),
-          const SizedBox(
-            height: 20,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.style14.copyWith(fontWeight: FontWeight.w400),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                const CustomAppBarBookDetails(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .19),
+                  child: const CustomBookImage(),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Text(
+                  'The Jungle Book',
+                  style: Styles.style30,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'The Jungle Book',
+                  style: Styles.style18.copyWith(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white.withOpacity(.7)),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookReating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const BooksAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 40,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.style14.copyWith(fontWeight: FontWeight.w400),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const SimilarBoxListView(),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          const SimilarBoxListView(),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
-      ),
+        )
+      ],
     );
+    // return
   }
 }
