f87c6d846cb506a4b74dd803c32a1dce3fcd7bb3
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-13 17:33:06 +0200

    searchRusert

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly1/Features/home/data/presentation/views/widget/best_seller_list_view_item.dart';
 import 'package:bookly1/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly1/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -9,10 +11,40 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomTextField(),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Search Result',
+            style: Styles.style20,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(child: RuseltSearchListView()),
         ],
       ),
     );
+  }
+}
+
+class RuseltSearchListView extends StatelessWidget {
+  const RuseltSearchListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        // shrinkWrap: true,
+        padding: EdgeInsets.zero,
+        // itemCount: 10,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: BookListViewItem(),
+          );
+        });
   }
 }
