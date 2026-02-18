f5f7a7ec4c95a63df6b1d026d82c1e10b937517b
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-18 17:08:55 +0200

    create home rabo methods

--- lib/Features/home/data/reops/home_repo.dart
+++ lib/Features/home/data/reops/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly1/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly1/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+ Future<Either<Failures,List<BookModel>>> fetchBestSellerBooks(); //Eitherمكتبة جاية من dartz وديا اضافة بتخليني اضيف 2 رتيرن
+  Future<Either<Failures,List<BookModel>>> fetchFeaturedBooks();
 }
---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,5 @@
+abstract class Failures {
+  
+}
+
+class ServerFailure extends Failures{}
