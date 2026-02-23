2e1f0972e75848f92f0da770c4497e01b47397f3
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-23 07:16:17 +0200

    creeat featuerd books

--- lib/Features/home/presentation/manger/feauterd_books_cubit/featuerd_books_cubit.dart
+++ lib/Features/home/presentation/manger/feauterd_books_cubit/featuerd_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly1/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly1/Features/home/data/reops/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featuerd_books_state.dart';
 
 class FeatuerdBooksCubit extends Cubit<FeatuerdBooksState> {
-  FeatuerdBooksCubit() : super(FeatuerdBooksInitial());
+  FeatuerdBooksCubit(this.homeRepo) : super(FeatuerdBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future featchFeatuerdBooks() async {
+    emit(FeatuerdBooksLoding());
+    var result = await homeRepo.fetchFeaturedBooks();  
+    result.fold((feailuer) {
+      emit(FeatuerdBooksFailuer(feailuer.errMessage));
+    }, (books) {
+      emit(FeatuerdBooksSuccess(books));
+    });
+  }
 }
