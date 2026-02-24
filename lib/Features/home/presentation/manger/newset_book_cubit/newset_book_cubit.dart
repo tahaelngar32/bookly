11dff3026286cbc7b47b92a4b1ea06b319e7548b
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-24 07:03:10 +0200

    newset books cubit done

--- lib/Features/home/presentation/manger/newset_book_cubit/newset_book_cubit.dart
+++ lib/Features/home/presentation/manger/newset_book_cubit/newset_book_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly1/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly1/Features/home/data/reops/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_book_state.dart';
 
 class NewsetBookCubit extends Cubit<NewsetBookState> {
-  NewsetBookCubit() : super(NewsetBookInitial());
+  NewsetBookCubit(this.homeRepo) : super(NewsetBookInitial());
+
+  final HomeRepo homeRepo;
+  Future featchNewsetBooks() async {
+    emit(NewsetBookLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold((feailuer) {
+      emit(NewsetBookFailuer(feailuer.errMessage));
+    }, (books) {
+      emit(NewsetBookSuccess(books));
+    });
+  }
 }
