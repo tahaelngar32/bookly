39438dbb1152be1670c8e1a71890613767b21310
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2026-02-24 21:25:52 +0200

    add stup service locaitor

---
+++ lib/core/utils/servies_locaitor.dart
@@ -0,0 +1,13 @@
+import 'package:bookly1/Features/home/data/reops/home_repo_impl.dart';
+import 'package:bookly1/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocaitor() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+    getIt.get<ApiService>(),
+  ));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:bookly1/Features/home/data/reops/home_repo_impl.dart';
+import 'package:bookly1/Features/home/presentation/manger/feauterd_books_cubit/featuerd_books_cubit.dart';
+import 'package:bookly1/Features/home/presentation/manger/newset_book_cubit/newset_book_cubit.dart';
 import 'package:bookly1/Features/home/presentation/views/home_view.dart';
 import 'package:bookly1/constens.dart';
+import 'package:bookly1/core/utils/api_service.dart';
 import 'package:bookly1/core/utils/app_router.dart';
+import 'package:bookly1/core/utils/servies_locaitor.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'Features/splash/presentation/views/splash_view.dart';
 
 void main() {
+  setupServiceLocaitor(); // جاية من مكتبة get
   runApp(const BooklyApp());
 }
 
@@ -16,15 +24,31 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.route,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-        // ThemeData.dark().textTheme عشان يفهم ان الثيم بتاعي دارك
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeatuerdBooksCubit(
+            getIt.get<
+                HomeRepoImpl>(), //servies_locaitor موجوده في الكلاس ده  مكتبة لاختصار الكود
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewsetBookCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.route,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+          // ThemeData.dark().textTheme عشان يفهم ان الثيم بتاعي دارك
+        ),
+        // home: const SplashView(),
       ),
-      // home: const SplashView(),
     );
   }
 }
